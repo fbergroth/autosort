d71b2f3b8943465ebe04aa9926cba0159402da96
Fredrik Bergroth <fbergroth@gmail.com>
2014-06-22 21:01:23 +0200

    Remove leading empty line in multiline test

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -6,7 +6,7 @@ from autosort.sorting import sort_imports
 
 def test_regular():
     path = os.path.abspath('test.py')
-    rv = sort_imports(dedent('''
+    rv = sort_imports(dedent('''\
     from tokenize import COMMENT, INDENT, ENDMARKER
     from tokenize import (DEDENT,  # noqa
                           NEWLINE, STRING,
@@ -16,5 +16,4 @@ def test_regular():
     assert rv == dedent('''\
     from tokenize import (COMMENT, DEDENT, ENDMARKER,     # noqa
                           INDENT, NAME, NEWLINE, STRING)  # noqa
-
     ''')

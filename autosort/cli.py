d906c7e6cc45873ceb22f08957c424f5338fb4dc
Fredrik Bergroth <fbergroth@gmail.com>
2014-06-23 15:29:39 +0200

    Add verbose output option

--- autosort/cli.py
+++ autosort/cli.py
@@ -7,6 +7,8 @@ from .sorting import sort_imports
 def create_parser():
     parser = argparse.ArgumentParser(prog='autosort')
     parser.add_argument('files', nargs='+')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='show verbose output')
     return parser
 
 
@@ -20,6 +22,14 @@ def main():
     args = parse_args(sys.argv[1:])
     for file in args.files:
         with open(file) as f:
-            output = sort_imports(f.read(), file)
-        with open(file, 'w') as f:
-            f.write(output)
+            input = f.read()
+
+        output = sort_imports(input, file)
+        changed = input != output
+
+        if changed:
+            with open(file, 'w') as f:
+                f.write(output)
+
+        if args.verbose:
+            print('{0} {1}'.format('>>>' if changed else '...', file))
